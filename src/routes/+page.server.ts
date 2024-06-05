6720be9baee0299075209f5015ca489bc63f655d
János Szathmáry <29057504+janory@users.noreply.github.com>
2024-06-05 13:00:39 +0200

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,6 @@ import blurhash from "@mux/blurhash"
 import type { ServerLoad } from "@sveltejs/kit"
 
 export const load: ServerLoad = async () => {
-  const muxPlaybackId = "svvqOj01YsD02Hkhd0267CAB1B02GoeL4eS2TsJ4cN1vlwk"
+  const muxPlaybackId = "3fevCt00ntwf7WxwvBhRo1EZ01IoABwo2d"
   return await blurhash(muxPlaybackId)
 }
